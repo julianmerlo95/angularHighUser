c389dea60a319c70ca4cf277e4b62177780f52ac
Julian <julimerlo95@gmail.com>
2020-10-03 18:52:35 -0300

    feat: added dependencies, styles and environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,18 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+const API_ANDINA = 'https://servicios.qamercantilandina.com.ar/api/v1';
+const API_MOCK = 'https://servicios.qamercantilandina.com.ar/api_mock_frontend/v1'
+const API_LOCATION = 'https://apis.datos.gob.ar/georef/api';
 
 export const environment = {
-  production: false
+  production: false,
+  vehicles: {
+    marca: `${API_ANDINA}/vehiculos/marcas`,
+    modelo: `${API_ANDINA}/vehiculos/marcas`,
+    version: `${API_ANDINA}/vehiculos/marcas`,
+  },
+  user: `${API_MOCK}/usuarios?nombre=`,
+  coverage: `${API_MOCK}/coberturas`,
+  location:{
+    provincias: `${API_LOCATION}/provincias`,
+    municipios: `${API_LOCATION}/municipios`,
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
