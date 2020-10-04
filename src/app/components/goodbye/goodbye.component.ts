af2248cdb91c83b5ab8adfd903ce99e88d75fcb7
Julian <julimerlo95@gmail.com>
2020-10-04 17:26:19 -0300

    fix: update styles at scss for eslint, goodbye

--- src/app/components/goodbye/goodbye.component.ts
+++ src/app/components/goodbye/goodbye.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-goodbye',
   templateUrl: './goodbye.component.html',
-  styleUrls: ['./goodbye.component.sass']
+  styleUrls: ['./goodbye.component.scss']
 })
 export class GoodbyeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goBack(): any{
+    this.router.navigate(['/home']);
   }
 
 }
