00f4bbd484f0d2c01276795a35820a6b327d59c5
Julian <julimerlo95@gmail.com>
2020-10-04 17:26:55 -0300

    fix: update styles at scss for eslint, home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  img: any;
+
+  constructor(private router: Router) {
+    this.img = '../../../assets/img/imgSeguro.jpg';
+    console.log(this.img);
+
+  }
 
   ngOnInit(): void {
+  }
+
+  nextStep(): any{
+    this.router.navigate(['/high/information']);
   }
 
 }
