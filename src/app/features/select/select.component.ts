e655b7555efb0321d3252460d47d2b3848a5ec07
Julian <julimerlo95@gmail.com>
2020-10-04 17:28:37 -0300

    fix: update style input, select and summary

--- src/app/features/select/select.component.ts
+++ src/app/features/select/select.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ValidatorsService } from 'src/app/services/validators.service';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.sass']
+  styleUrls: ['./select.component.scss']
 })
 export class SelectComponent implements OnInit {
 
-  @Input() elements
-  @Input() form
-  @Input() labelName
-  @Input() name
-  @Input() item
-  @Input() itemTwo
+  @Input() elements;
+  @Input() form;
+  @Input() labelName;
+  @Input() name;
+  @Input() item;
+  @Input() itemTwo;
 
-  constructor() { }
+  constructor(private validatorService: ValidatorsService) {
+  }
 
   ngOnInit(): void {
   }
